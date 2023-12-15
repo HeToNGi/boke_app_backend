e212da6bea15cb6f036e282e4263c7e0595c618d
hetong <1061459128@qq.com>
2023-12-15 11:05:46 +0800

    添加鉴权认证

--- index.js
+++ index.js
@@ -179,11 +179,22 @@ app.get('/stock_price', (req, res) => {
   });
 });
 
+let access_token = '24.3a752bad7b97f23cc087b70c182b2852.2592000.1705200589.282335-41731834';
+// let access_token = '24.4cdd94cd61a959652b52538642ee3d1d.2592000.1700815691.282335-41731833';
 app.post('/wenxinworkshop', (req, res) => {
   const { messages } = req.body;
-  apiClient.post('/rpc/2.0/ai_custom/v1/wenxinworkshop/chat/completions_pro?access_token=24.4cdd94cd61a959652b52538642ee3d1d.2592000.1700815691.282335-41731833', {
+  apiClient.post('/rpc/2.0/ai_custom/v1/wenxinworkshop/chat/completions_pro?access_token=' + access_token, {
     "messages": messages
   }).then(response => {
+    const { error_code } = response.data;
+    if (error_code && error_code === 111) {
+      apiClient.post('/oauth/2.0/token?client_id=RAzDbN1aPg2vzpTEA1qZTyPB&client_secret=5vbU1fqGtXqiQbjNe81xavuQ8Tedh6fA&grant_type=client_credentials').then((r) => {
+        access_token = r.data.access_token;
+        console.log('认证过期', new Date())
+        res.send({code: 0, message: '请求成功', data: {result: '抱歉，可以把问题描述的在完整一些么'}});
+      })
+      return;
+    }
     res.send({code: 0, message: '请求成功', data: {result: response.data.result || '抱歉，可以把问题描述的在完整一些么'}});
   }).catch((err) => {
     res.send({code: -1, message: '请求失败', data: {result: '服务超时', err,}});
